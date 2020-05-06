635cd5d517707b37eb3a76b80d76427aef09a6e6
saerdem <saerdem@gmail.com>
2020-05-06 22:08:55 +0300

    add 3.file by Intellij

---
+++ src/gitLearn/git3rdFileByIntellij.java
@@ -0,0 +1,7 @@
+package gitLearn;
+
+public class git3rdFileByIntellij {
+    public static void main(String[] args) {
+        System.out.println("This is pushed by Intellij with ALL files together");
+    }
+}
